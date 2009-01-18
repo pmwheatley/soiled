da4f19ab845c393ecbf1649adb14532ce4ee1788
bofh69 <bofh69@c1b1574c-8591-4b2f-8170-dace01c35af4>
2009-01-18 14:09:24 +0000

    git-svn-id: svn://localhost/soiled@27 c1b1574c-8591-4b2f-8170-dace01c35af4

--- trunk/src/CharBuffer.hx
+++ trunk/src/CharBuffer.hx
@@ -177,10 +177,133 @@ class CharBuffer extends Bitmap {
 	    var b = s.charCodeAt(i);
 	    if(b == 10) {
 		carriageReturn();
-		this.lineFeed();
+		lineFeed();
 	    } else printChar(b);
 	}
 	endUpdate();
+    }
+
+    private function findBreakPoint(s : String, start : Int, width : Int) : Int
+    {
+	var lastWrapPoint = start;
+	var pos = start;
+	var currentWidth = 0;
+	while(pos < s.length) {
+	    var c = s.charCodeAt(pos);
+	    pos++;
+	    if(c == 0x0A) { // LF
+		return pos-1;
+	    }
+	    if(c == 0x200B) { // Zero width space
+		lastWrapPoint = pos-1;
+	    } else {
+		// Se if it is a soft hypen or space:
+		if(c == 32 ||
+	           c == 0x00AD) lastWrapPoint = pos-1;
+		currentWidth++;
+		if(currentWidth == width) {
+		    if(lastWrapPoint != (pos-1) &&
+		       pos < s.length &&
+		       s.charCodeAt(pos) == 32) {
+			// If the last character is a space,
+			// we can wrap here too.
+			lastWrapPoint = pos;
+		    }
+		    if(lastWrapPoint == start) {
+			return pos;
+		    } else {
+			var c = s.charCodeAt(lastWrapPoint);
+			if(c == 0x00AD) { // Soft hypen.
+			    return lastWrapPoint + 1;
+			}
+			return lastWrapPoint;
+		    }
+		}
+	    }
+	}
+	return pos;
+    }
+
+
+    /* Writes the text, word wrapped to fit within maxWidth columns.
+     * TODO Add support for \n
+     * Returns the lines.
+     * 
+     * The function handles:
+     * Soft hyphen (U+00AD)
+     * Non-breaking hyphen (U+2011)
+     * No-break space (U+00A0)
+     * Zero-width space (U+200B)
+     */
+    public function wordWrapText(s : String,
+	                         firstLineIndent : Int,
+				 lineIndent : Int,
+				 maxWidth : Int) : Array<String>
+    {
+	var pos : Int = 0;
+    	var result : Array<String> = new Array();
+
+	while(true) {
+	    var oldPos = pos;
+	    while(pos < s.length &&
+		    (s.charCodeAt(pos) == 0x20 ||
+		     s.charCodeAt(pos) == 0x200B)) pos++;
+	    if(pos < s.length && s.charCodeAt(pos) == 0x0A) {
+		pos++;
+		result.push("");
+	    }
+	    if(oldPos == pos) break;
+	}
+
+	var lines = 0;
+
+	while(pos < s.length) {
+	    lines++;
+	    var indent = lineIndent;
+	    if(lines == 1) {
+		indent = firstLineIndent;
+	    }
+	    var start = pos;
+	    var end = findBreakPoint(s, start, maxWidth-indent);
+	    var buff = new StringBuf();
+	    for(i in 0 ... indent) buff.addChar(32);
+	    for(i in start...end) {
+		var c = s.charCodeAt(i);
+		if(c != 0x200B)
+		    buff.addChar(c);
+	    }
+	    result.push(buff.toString());
+	    pos = end;
+	    if(pos < s.length && s.charCodeAt(pos) == 0x0A) {
+		lines = 0;
+		pos++;
+	    }
+	    while(true) {
+		var oldPos = pos;
+		while(pos < s.length &&
+			(s.charCodeAt(pos) == 0x20 ||
+			 s.charCodeAt(pos) == 0x200B)) pos++;
+		if(pos < s.length && s.charCodeAt(pos) == 0x0A) {
+		    pos++;
+		    lines=0;
+		    result.push("");
+		}
+		if(oldPos == pos) break;
+	    }
+	}
+	return result;
+    }
+
+    public function printWordWrap(s : String)
+    {
+	var rows = wordWrapText(s, 0, 4, columns);
+	for(i in 0...rows.length) {
+	    for(j in 0 ... rows[i].length) {
+		printChar(rows[i].charCodeAt(j));
+	    }
+	    carriageReturn();
+	    lineFeed();
+	}
     }
 
     public function printChar(b : Int)
@@ -490,6 +613,30 @@ class CharBuffer extends Bitmap {
 	updateTmpSelectionBuffer(true);
 
 	endUpdate();
+    }
+
+    public function getWordAt(x : Int, y : Int) : String
+    {
+	scrollbackToBottom(); // XXX Until it works better...
+
+	var startPos = x + y * columns;
+
+	if(charBuffer[startPos] > 32) {
+	    var endPos = startPos;
+	    while(startPos > 1 && charBuffer[startPos-1] > 32) {
+		startPos--;
+	    }
+	    while(endPos < rows*columns-1 && charBuffer[endPos+1] > 32) {
+		endPos++;
+	    }
+	    var buff = new StringBuf();
+	    for(i in startPos ... endPos+1) {
+		buff.addChar(charBuffer[i]);
+	    }
+	    return buff.toString();
+	} else {
+	    return "";
+	}
     }
 
     public function doCopy() : Bool
