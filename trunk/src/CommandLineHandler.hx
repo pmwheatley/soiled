e5892d12d8117df7c516e85754ac0bbd6021ba75
bofh69 <bofh69@c1b1574c-8591-4b2f-8170-dace01c35af4>
2009-10-11 09:44:53 +0000

    Minor refactoring: broke up the HandleLocalCommand function into smaller functions... Should be moved to their own class(es?).
    git-svn-id: svn://localhost/soiled@47 c1b1574c-8591-4b2f-8170-dace01c35af4

--- trunk/src/CommandLineHandler.hx
+++ trunk/src/CommandLineHandler.hx
@@ -276,174 +276,19 @@ class CommandLineHandler
     private function handleLocalCommands(cmd : String, first : Int, last : Int)
     {
 	if(cmd == "/help" || cmd == "/?" ) {
-	    var args = getArgs(last);
-	    if(args.length == 0) {
-		appendText(
-			"\nLocal commands start with a '/' character. If you really want to send such a command to the server, write an extra '/' character before it.\n" +
-			"\n" +
-			"Known commands are:\n" +
-			"/addinput <text> - adds <text> to the input buffer.\n" +
-			"/alias - lists defined aliases.\n" +
-			"/alias <name> - displays the <name> alias' <value>.\n" +
-			"/alias <name> <value> - binds <name> as an alias to <value>.\n" +
-			"/help <topic> - read about <topic>.\n" +
-			"/help topics - a list of available topics.\n" +
-			"/set - list defined variables.\n" +
-			"/set <name> - displays the variable's value.\n" +
-			"/set <name> <value> - sets the variable.\n" +
-			"/unalias <name> - removes the alias.\n" +
-			"/unset <name> - removes the variable.");
-	    } else {
-		switch(args) {
-		    case "aliases":
-			appendText(
-				"\n" +
-				"Aliases\n" +
-				"-------\n" +
-				"Aliases substitute a written command for some other text.\n\n" +
-				"An example:\n" +
-			        "# /alias test say Test!\n" +
-			        "# test Nisse\n" +
-			        "You say Test! Nisse\n\n" +
-				"One alias can send two or more commands to the server, just separete them with %; like this:\n" +
-				"# /alias test say Hi! %; say\n" +
-				"# test Ho!\n" +
-				"You say Hi!\n" +
-				"You say Ho!\n" +
-				"\n" +
-				"If you want your alias to send a single % character, write %% instead.\n"
-				);
-		    case "keys":
-			appendText(
-				"\n" +
-				"CTRL + A - Move cursor to the beginning of the line.\n" +
-				"CTRL + E - Move cursor to the end of the line.\n" +
-				"CTRL + B - Move cursor one character to the left/back.\n" +
-				"CTRL + F - Move cursor one character to the right/forward.\n" +
-				"CTRL + D/DEL - Delete one character under the cursor.\n" +
-				"CTRL + H/BACKSPACE - Delete the character to the left of the cursor.\n" +
-				"CTRL + W - Delete the word to the left of the cursor.\n" +
-				"CTRL + U - Delete to the beginning of the line.\n" +
-				"CTRL + K - Delete to the end of the line.\n" +
-				"CTRL + L - Clear the whole screen.\n" +
-				"CTRL + M/ENTER - Send the written text.\n" +
-				"CTRL + P - Exchange the input for the previous input line in the history.\n" +
-				"CTRL + N - Exchange the input for the next input line in the history.\n" +
-				"SHIFT + PAGE UP/DOWN - scroll back to previous text.\n\n" +
-				"Pressing COMMAND/CTRL + left mouse button on a URL opens it.\n");
-		    case "macros":
-			appendText(
-				"\n" +
-				"Macros\n" +
-				"------\n" +
-				"Macros are commands bound to a single command. You use the /alias command to define them, but they do not work like aliases, they work like you had written the alias' text directly on the input line.\n" +
-				"The name of a macro is of the format KEY_<name>[_[S][C][A]]\n" +
-				"The name is one of HOME, END, INSERT, F1..F12, PGUP, PGDN, UP, DOWN, LEFT or RIGHT and the _S, _C or _A suffixes (and combinations like _SA) are used when the key is pressed together with shift, ctrl or alt/option.\n" +
-				"\n" +
-				"An example:\n" +
-				"# /alias KEY_F1 /help\n" +
-				"# /alias KEY_F1_S //help\n" +
-				"# /alias KEY_F1_SCA help\n" +
-				"\n" +
-				"Will cause \"/help\" to be entered when you press F1, \"/help\" is sent to\n the server when you press shift+F1 and \"help\" will be sent when you press shift+ctrl+alt+F1. Please note that the order of SCA is important if you use combinations and not all combiations/characters are possible to enter.\n" +
-				"\r\n" +
-				"If a macro isn't defined for a key, but the variable \"LOCAL_EDIT\" is set to \"on\", then some internal function may be used.\n" +
-				"If \"LOCAL_EDIT\" isn't defined, a control sequence will be sent to the server."
-				);
-		    case "vars", "variables":
-			appendText(
-				"\n" +
-				"Variables\n" +
-				"---------\n" +
-				"Variables hold a value. Currently they are used for changing the way the client works. The known variables are:\n" +
-			       	"LANG: What language the OS is set to. Only set once.\n" +
-			       	"LOCAL_EDIT: When set to \"on\", some of the keys are used for local line editing instead of being sent to the server.\n" +
-			       	"OS: What platform the client is running on. Only set once.\n" +
-				"");
-		    default:
-			appendText(
-				"\nAvailable topics are:" +
-				"\naliases - how aliases work." +
-				"\nmacros - how macros work." +
-				"\nkeys - the key bindings." +
-				"\ntopics - this list." +
-				"\nvariables - how variables work/built in variables." +
-				"");
-		}
-	    }
+	    handleCmdHelp(last);
 	} else if(cmd == "/addinput") {
-	    cmd = StringTools.trim(inputString.substr(last));
-	    if(cmd.length == 0) {
-		appendText("\n%add what input?");
-	    } else {
-		if(oldPosition == oldInput.length) {
-		    oldInput += cmd;
-		} else {
-		    var s = oldInput.substr(0, oldPosition) +
-			cmd +
-			oldInput.substr(oldPosition, oldInput.length - oldPosition);
-		    oldInput = s;
-		}
-		oldPosition += cmd.length;
-		return false;
-	    }
+	    return handleCmdAddInput(last);
 	} else if(cmd == "/alias") {
-	    cmd = StringTools.trim(inputString.substr(last));
-	    if(cmd.length == 0) {
-		for(alias in config.getAliases().keys()) {
-		    appendText("\n/alias " + alias + " " + config.getAliases().get(alias));
-		}
-	    } else {
-		first = cmd.indexOf(" ");
-		if(first == -1) {
-		    if(config.getAliases().exists(cmd)) {
-			appendText("\n/alias " + cmd + " " + config.getAliases().get(cmd));
-		    }
-		} else {
-		    inputString = StringTools.ltrim(cmd.substr(first));
-		    cmd = cmd.substr(0, first);
-		    config.getAliases().set(cmd, inputString);
-		    config.saveAliases();
-		}
-	    }
+	    handleCmdAlias(first, last);
 	} else if(cmd == "/save") {
-	    config.save();
-	    appendText("\n%Saved config.");
+	    handleCmdSave();
 	} else if(cmd == "/set") {
-	    cmd = StringTools.trim(inputString.substr(last));
-	    if(cmd.length == 0) {
-		for(v in config.getVars().keys()) {
-		    appendText("\n/set " + v + " " + config.getVar(v));
-		}
-	    } else {
-		first = cmd.indexOf(" ");
-		if(first == -1) {
-		    if(config.getVars().exists(cmd)) {
-			appendText("\n/set " + cmd + " " + config.getVar(cmd));
-		    }
-		} else {
-		    inputString = StringTools.ltrim(cmd.substr(first));
-		    cmd = cmd.substr(0, first);
-		    config.setVar(cmd, inputString);
-		    config.saveVars();
-		}
-	    }
+	    handleCmdSet(last);
 	} else if(cmd == "/unalias") {
-	    cmd = StringTools.trim(inputString.substr(last));
-	    if(!config.getAliases().exists(cmd)) {
-		appendText("\n% unalias: \"" + cmd + "\": no such alias");
-	    } else {
-		config.getAliases().remove(cmd);
-		config.saveAliases();
-	    }
+	    handleCmdUnalias(last);
 	} else if(cmd == "/unset") {
-	    cmd = StringTools.trim(inputString.substr(last));
-	    if(config.getVar(cmd) == null) {
-		appendText("\n% unset: \"" + cmd + "\": no such variable");
-	    } else {
-		config.getVars().remove(cmd);
-		config.saveVars();
-	    }
+	    handleCmdUnset(last);
 	} else {
 	    appendText("\n% " + cmd.substr(1) + ": no such command or macro");
 	}
@@ -451,6 +296,197 @@ class CommandLineHandler
 	cb.lineFeed();
 	cb.setExtraCurs(cb.getCursX(), cb.getCursY());
 	return true;
+    }
+
+    private function handleCmdHelp(last)
+    {
+	var args = getArgs(last);
+	if(args.length == 0) {
+	    appendText(
+		    "\nLocal commands start with a '/' character. If you really want to send such a command to the server, write an extra '/' character before it.\n" +
+		    "\n" +
+		    "Known commands are:\n" +
+		    "/addinput <text> - adds <text> to the input buffer.\n" +
+		    "/alias - lists defined aliases.\n" +
+		    "/alias <name> - displays the <name> alias' <value>.\n" +
+		    "/alias <name> <value> - binds <name> as an alias to <value>.\n" +
+		    "/help <topic> - read about <topic>.\n" +
+		    "/help topics - a list of available topics.\n" +
+		    "/set - list defined variables.\n" +
+		    "/set <name> - displays the variable's value.\n" +
+		    "/set <name> <value> - sets the variable.\n" +
+		    "/unalias <name> - removes the alias.\n" +
+		    "/unset <name> - removes the variable.");
+	} else {
+	    switch(args) {
+		case "aliases":
+		    appendText(
+			    "\n" +
+			    "Aliases\n" +
+			    "-------\n" +
+			    "Aliases substitute a written command for some other text.\n\n" +
+			    "An example:\n" +
+			    "# /alias test say Test!\n" +
+			    "# test Nisse\n" +
+			    "You say Test! Nisse\n\n" +
+			    "One alias can send two or more commands to the server, just separete them with %; like this:\n" +
+			    "# /alias test say Hi! %; say\n" +
+			    "# test Ho!\n" +
+			    "You say Hi!\n" +
+			    "You say Ho!\n" +
+			    "\n" +
+			    "If you want your alias to send a single % character, write %% instead.\n"
+			    );
+		case "keys":
+		    appendText(
+			    "\n" +
+			    "CTRL + A - Move cursor to the beginning of the line.\n" +
+			    "CTRL + E - Move cursor to the end of the line.\n" +
+			    "CTRL + B - Move cursor one character to the left/back.\n" +
+			    "CTRL + F - Move cursor one character to the right/forward.\n" +
+			    "CTRL + D/DEL - Delete one character under the cursor.\n" +
+			    "CTRL + H/BACKSPACE - Delete the character to the left of the cursor.\n" +
+			    "CTRL + W - Delete the word to the left of the cursor.\n" +
+			    "CTRL + U - Delete to the beginning of the line.\n" +
+			    "CTRL + K - Delete to the end of the line.\n" +
+			    "CTRL + L - Clear the whole screen.\n" +
+			    "CTRL + M/ENTER - Send the written text.\n" +
+			    "CTRL + P - Exchange the input for the previous input line in the history.\n" +
+			    "CTRL + N - Exchange the input for the next input line in the history.\n" +
+			    "SHIFT + PAGE UP/DOWN - scroll back to previous text.\n\n" +
+			    "Pressing COMMAND/CTRL + left mouse button on a URL opens it.\n");
+		case "macros":
+		    appendText(
+			    "\n" +
+			    "Macros\n" +
+			    "------\n" +
+			    "Macros are commands bound to a single command. You use the /alias command to define them, but they do not work like aliases, they work like you had written the alias' text directly on the input line.\n" +
+			    "The name of a macro is of the format KEY_<name>[_[S][C][A]]\n" +
+			    "The name is one of HOME, END, INSERT, F1..F12, PGUP, PGDN, UP, DOWN, LEFT or RIGHT and the _S, _C or _A suffixes (and combinations like _SA) are used when the key is pressed together with shift, ctrl or alt/option.\n" +
+			    "\n" +
+			    "An example:\n" +
+			    "# /alias KEY_F1 /help\n" +
+			    "# /alias KEY_F1_S //help\n" +
+			    "# /alias KEY_F1_SCA help\n" +
+			    "\n" +
+			    "Will cause \"/help\" to be entered when you press F1, \"/help\" is sent to\n the server when you press shift+F1 and \"help\" will be sent when you press shift+ctrl+alt+F1. Please note that the order of SCA is important if you use combinations and not all combiations/characters are possible to enter.\n" +
+			    "\r\n" +
+			    "If a macro isn't defined for a key, but the variable \"LOCAL_EDIT\" is set to \"on\", then some internal function may be used.\n" +
+			    "If \"LOCAL_EDIT\" isn't defined, a control sequence will be sent to the server."
+			    );
+		case "vars", "variables":
+		    appendText(
+			    "\n" +
+			    "Variables\n" +
+			    "---------\n" +
+			    "Variables hold a value. Currently they are used for changing the way the client works. The known variables are:\n" +
+			    "LANG: What language the OS is set to. Only set once.\n" +
+			    "LOCAL_EDIT: When set to \"on\", some of the keys are used for local line editing instead of being sent to the server.\n" +
+			    "OS: What platform the client is running on. Only set once.\n" +
+			    "");
+		default:
+		appendText(
+			"\nAvailable topics are:" +
+			"\naliases - how aliases work." +
+			"\nmacros - how macros work." +
+			"\nkeys - the key bindings." +
+			"\ntopics - this list." +
+			"\nvariables - how variables work/built in variables." +
+			"");
+	    }
+	}
+    }
+
+    private function handleCmdAddInput(last : Int) : Bool
+    {
+	var cmd = StringTools.trim(inputString.substr(last));
+	if(cmd.length == 0) {
+	    appendText("\n%add what input?");
+	} else {
+	    if(oldPosition == oldInput.length) {
+		oldInput += cmd;
+	    } else {
+		var s = oldInput.substr(0, oldPosition) +
+		    cmd +
+		    oldInput.substr(oldPosition, oldInput.length - oldPosition);
+		oldInput = s;
+	    }
+	    oldPosition += cmd.length;
+	    return false;
+	}
+	return true;
+    }
+
+    private function handleCmdAlias(first : Int, last : Int)
+    {
+	var cmd = StringTools.trim(inputString.substr(last));
+	if(cmd.length == 0) {
+	    for(alias in config.getAliases().keys()) {
+		appendText("\n/alias " + alias + " " + config.getAliases().get(alias));
+	    }
+	} else {
+	    first = cmd.indexOf(" ");
+	    if(first == -1) {
+		if(config.getAliases().exists(cmd)) {
+		    appendText("\n/alias " + cmd + " " + config.getAliases().get(cmd));
+		}
+	    } else {
+		inputString = StringTools.ltrim(cmd.substr(first));
+		cmd = cmd.substr(0, first);
+		config.getAliases().set(cmd, inputString);
+		config.saveAliases();
+	    }
+	}
+    }
+
+    private function handleCmdSave()
+    {
+	config.save();
+	appendText("\n%Saved config.");
+    }
+
+    private function handleCmdSet(last : Int)
+    {
+	var cmd = StringTools.trim(inputString.substr(last));
+	if(cmd.length == 0) {
+	    for(v in config.getVars().keys()) {
+		appendText("\n/set " + v + " " + config.getVar(v));
+	    }
+	} else {
+	    var first = cmd.indexOf(" ");
+	    if(first == -1) {
+		if(config.getVars().exists(cmd)) {
+		    appendText("\n/set " + cmd + " " + config.getVar(cmd));
+		}
+	    } else {
+		inputString = StringTools.ltrim(cmd.substr(first));
+		cmd = cmd.substr(0, first);
+		config.setVar(cmd, inputString);
+		config.saveVars();
+	    }
+	}
+    }
+
+    private function handleCmdUnalias(last : Int)
+    {
+	var cmd = StringTools.trim(inputString.substr(last));
+	if(!config.getAliases().exists(cmd)) {
+	    appendText("\n% unalias: \"" + cmd + "\": no such alias");
+	} else {
+	    config.getAliases().remove(cmd);
+	    config.saveAliases();
+	}
+    }
+
+    private function handleCmdUnset(last : Int)
+    {
+	var cmd = StringTools.trim(inputString.substr(last));
+	if(config.getVar(cmd) == null) {
+	    appendText("\n% unset: \"" + cmd + "\": no such variable");
+	} else {
+	    config.getVars().remove(cmd);
+	    config.saveVars();
+	}
     }
 
     private inline function isCharByCharMode() : Bool
